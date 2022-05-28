3476adc5403138d6a4771c36b593450f67b34385
golu701 <golurajak811@gmail.com>
2022-05-28 19:39:22 +0530

    New: images, fonts added, getstarted screen added color, font and image index file added

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native'
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { GetStarted } from '../screens';
+const Stack = createNativeStackNavigator();
+
+const Router = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="GetStarted" component={GetStarted} options={{ headerShown: false }} />
+        </Stack.Navigator>
+    )
+}
+
+export default Router;
+
+const styles = StyleSheet.create({})
---
+++ src/screens/GetStarted/index.js
@@ -0,0 +1,33 @@
+import { StyleSheet, Text, View, Image } from 'react-native';
+import React from 'react';
+import { IL_GetStarted_PNG } from '../../res/images/Illustrations';
+import { colors, fonts } from '../../res';
+
+const GetStarted = () => {
+  return (
+    <View style={styles.screen}>
+      <Image source={IL_GetStarted_PNG} style={styles.image} />
+      <View style={styles.wrapperSlogan}>
+        <Text style={styles.textSlogan}>Shop Your Daily</Text>
+        <Text style={styles.textSlogan}>Necessary</Text>
+      </View>
+      <View style={{ backgroundColor: colors.primary, height: 50, width: 259, borderRadius: 20, justifyContent: 'center', alignSelf: 'center' }}>
+        <Text style={{ fontSize: 18, fontFamily: fonts.Medium, color: colors.white, textAlign: 'center' }}>Get Started</Text>
+      </View>
+    </View>
+  )
+}
+
+export default GetStarted
+
+const styles = StyleSheet.create({
+  screen: { flex: 1, paddingHorizontal: 20, justifyContent: 'center' },
+  image: { height: 225, width: '100%', resizeMode: 'stretch' },
+  wrapperSlogan: { marginTop: 51 },
+  textSlogan: {
+    fontSize: 30,
+    color: colors.primary,
+    textAlign: 'center',
+    fontFamily: fonts.SemiBold
+  }
+});
